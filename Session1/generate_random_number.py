f52e2f44465efc52962edcbf519fa1e1086b7606
Rohit Sharma <rohit.sharma1048@gmail.com>
2023-01-16 13:57:21 -0600

    add generate_random_number.py

---
+++ Session1/generate_random_number.py
@@ -0,0 +1,9 @@
+# import random library/ package
+import random
+
+# get a random number by calling randint method from random package
+# randint is a method that Return random integer in range [a, b], including both end points.
+randomNumber = random.randint(1, 1000)
+
+# Now we print out rando, number
+print(f"Generated Random Number is {randomNumber}")
