7be2c3af19a44d26731ffe2d19e4e903cdca9499
Rohit Sharma <rohit.sharma1048@gmail.com>
2023-01-16 14:02:43 -0600

    add problem 1

---
+++ Session1/you-win-or-i-win.py
@@ -0,0 +1,28 @@
+# import random library/ package
+import random
+
+# get a random number by calling randint method from random package
+# randint is a method that Return random integer in range [a, b], including both end points.
+randomNumber = random.randint(1, 1000)
+
+# Now we print out rando, number
+print(f"Generated Random Number is {randomNumber}")
+
+# add logic to compare if numner is even or odd
+# if number is even, print "You win" otherwise print "I Win"
+
+# you can use modulo (%) operator to find if number is divisible by 2
+# a number is divisible by 2 it is even
+# use can use modulo operator to check if number is even or odd
+# a % b gives you remainder when you divide a by b
+'''
+this code will help you to come with your answer.
+
+if a % b == 0:
+    print("I win")
+else:
+    print("You win")
+'''
+
+# Add your code below...
+
