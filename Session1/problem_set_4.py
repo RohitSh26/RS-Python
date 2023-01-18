fa23978320a53af9cb6df9dd7b29bb8c6ad9beff
Rohit Sharma <rohit.sharma1048@gmail.com>
2023-01-18 14:27:41 -0600

    Create problem_set_4.py

---
+++ Session1/problem_set_4.py
@@ -0,0 +1,13 @@
+"""
+Write the output of the below program
+        
+    if (a > 5 and a <=10):    
+         print("Hello")    
+    else:    
+        print("Bye")
+        
+a. When a = 9
+b. When a = 5
+c. When a = 11
+d. When a = 10
+"""
