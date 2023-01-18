938d6843078ebf173b18eb26e6bb6e305de99948
Rohit Sharma <rohit.sharma1048@gmail.com>
2023-01-18 14:14:08 -0600

    Create problem_set_3.py

---
+++ Session1/problem_set_3.py
@@ -0,0 +1,14 @@
+"""
+Write a program to accept a number from 1 to 12 and display name of the month 
+and days in that month like 1 for January and number of days 31 and so on
+
+Exapmple:
+random number generated = 1 then print, Month is January and number of days is 31
+random number generated = 2 then print, Month is Februalry and number of days is 28
+...
+...
+
+To accept a number use random package, that will automatically generate a number between 1 and 12
+print month name and number of days in that month
+
+"""
