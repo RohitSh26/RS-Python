86a397c91ac01fa3efecf5b787d3f1882b477541
Rohit Sharma <rohit.sharma1048@gmail.com>
2023-01-18 14:04:34 -0600

    Create problem_set_2.py

---
+++ Session1/problem_set_2.py
@@ -0,0 +1,10 @@
+"""
+write lines of code which can find out if the number is even or odd
+
+"""
+
+# your code should print if test_number is even or odd
+test_number = 13
+
+# write code below. You have done something similar in problem set 1
+
